8108296de4a6dc61c0ce5d27b84c2ba9fbbb1e14
Ademir Júnior <adejr0220@gmail.com>
2025-04-02 21:26:46 -0300

    🎉 start: Setup Java Project, Implement A Basic Calculator For Testing And Update Readme

---
+++ ProjetoJava/src/App.java
@@ -0,0 +1,26 @@
+import SimpleCalculator.SimpleCalculator;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World! Let's do some calculations!");
+        try {
+            SimpleCalculator calculator = new SimpleCalculator();
+            double a = 10.0;
+            double b = 5.0;
+            double c = 8.0;
+            double d = 2.0;
+
+            double sum = calculator.add(a, b);
+            double difference = calculator.subtract(a, b);
+            double product = calculator.multiply(a, c);
+            double quotient = calculator.divide(c, d); // This will throw an exception
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Difference: " + difference);
+            System.out.println("Product: " + product);
+            System.out.println("Quotient: " + quotient);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Compilador-Java
+
 Este é um projeto feito na matéria de Compiladores na Universidade Estadual do Sudoeste da Bahia sob a orientação do Professor José Carlos Martins Oliveira.
+
+## Analisador Léxico
+
+...
